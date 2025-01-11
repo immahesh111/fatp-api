5d064eaba2ff9ac170cd9736998db0f679769013
immahesh555 <themahiece@gmail.com>
2025-01-11 15:30:35 +0530

    sdedf

--- routes/leave11.js
+++ routes/leave11.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import authMiddleware from '../middleware/authMiddleware.js'
-import { addLeave, getLeave, getLeaves, getLeaveDetail, updateLeave } from '../controllers/leaveController10.js'
+import { addLeave, getLeave, getLeaves, getLeaveDetail, updateLeave } from '../controllers/leaveController11.js'
 
 
 const router3 = express.Router()
